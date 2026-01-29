6df961e34dc945e80fc89175dba2b53e10a29a7a
YWY2002 <wenyuanyik@gmail.com>
2026-01-29 16:56:00 +0800

    Level 2. Add List

--- src/main/java/Tommy.java
+++ src/main/java/Tommy.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Tommy {
     public static void main(String[] args) {
@@ -14,12 +15,27 @@ public class Tommy {
         System.out.println("---------------------------------------");
         Scanner inputObj = new Scanner(System.in);
         String userInput = "";
+        ArrayList<String> inputList = new ArrayList<String>(100);   //creates array object(not list)
+ 
         do {
             userInput = inputObj.nextLine().trim();
             System.out.println("---------------------------------------");
-            if (userInput != "bye") {
-                System.out.println(userInput);
+
+            if (userInput.equals("list")) {
+                if (inputList.size() > 0) {
+                    for (int i = 0; i < inputList.size(); i++) {
+                        System.out.printf("%d. %s\n", i, inputList.get(i));
+                    }
+                }
+                else {
+                    System.out.printf("Your list is empty.");
+                }
             }
+            else if (!userInput.equals("bye")) {
+                inputList.add(userInput);
+                System.out.printf("added: %s\n", userInput);
+            }
+
             System.out.println("---------------------------------------");
         } while (!userInput.equalsIgnoreCase("bye"));
 
