801484387573c4bca51614fe881ccdde007a9794
YWY2002 <wenyuanyik@gmail.com>
2026-01-29 16:23:08 +0800

    Level 1. Echo

--- src/main/java/Tommy.java
+++ src/main/java/Tommy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Tommy {
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
@@ -7,9 +9,20 @@ public class Tommy {
         //         + "|____/ \\__,_|_|\\_\\___|\n";
         // System.out.println("Hello from\n" + logo);
         System.out.println("---------------------------------------");
-        System.out.println("Hello! I'm Tommy.");
+        System.out.println("Hello! I'm Tommy, your personal assistant!");
         System.out.println("What can I do for you?");
         System.out.println("---------------------------------------");
+        Scanner inputObj = new Scanner(System.in);
+        String userInput = "";
+        do {
+            userInput = inputObj.nextLine().trim();
+            System.out.println("---------------------------------------");
+            if (userInput != "bye") {
+                System.out.println(userInput);
+            }
+            System.out.println("---------------------------------------");
+        } while (!userInput.equalsIgnoreCase("bye"));
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("---------------------------------------");
 
