2280aa9d193b8c7f51f395015a3e3e8026c3be1f
YWY2002 <wenyuanyik@gmail.com>
2026-02-11 23:58:20 +0800

    Level 5. Handle Errors

--- src/main/java/Tommy.java
+++ src/main/java/Tommy.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Tommy {
@@ -13,84 +14,121 @@ public class Tommy {
         TaskManager taskManager = new TaskManager();
 
         do {
-            userInput = inputObj.nextLine().trim();
+            // userInput = inputObj.nextLine().trim();
             // Split command word from the rest of the sentence
-            String[] parts = userInput.split(" ", 2); 
-            String keyword = parts[0].toLowerCase();
-            String arguments = (parts.length > 1) ? parts[1] : "";
+            try {
+                userInput = inputObj.nextLine().trim();
+                String[] parts = userInput.split(" ", 2); 
+                String command = parts[0].toLowerCase();
+                String arguments = (parts.length > 1) ? parts[1] : "";
 
-            System.out.println("---------------------------------------");
-            
-            Task task;
-
-            switch(keyword) {
-                case "bye": 
-                    break;
-
-                case "list": 
-                    System.out.printf("Here are the tasks in your list:\n");
-                    taskManager.printAllTask();
-                    break;
-
-                case "mark":
-                    task = taskManager.findTask(Integer.parseInt(arguments));
-                    if (task != null) {
-                        task.setIsDone(true);
-                        System.out.printf("Nice! I have marked this task as done:\n%s\n", task.toString());
-                    }
-                    break;
-
-                case "unmark":
-                    task = taskManager.findTask(Integer.parseInt(arguments));
-                    if (task != null) {
-                        task.setIsDone(false);
-                        System.out.printf("Nice! I have marked this task as not done yet:\n%s\n", task.toString());
-                    }
-                    break;
-
-                case "todo":
-                    counter++;
-                    // arguments contains the description
-                    ToDo newTodo = new ToDo(counter, arguments, false);
-                    taskManager.addTask(newTodo);
-                    break;
-
-                case "deadline":
-                    counter++;
-                    // Split by " /by "
-                    // Example: "return book /by Sunday"
-                    String[] deadlineParts = arguments.split(" /by ");
-                    String deadlineDesc = deadlineParts[0];
-                    String by = deadlineParts[1];
-                    
-                    Deadline newDeadline = new Deadline(counter, deadlineDesc, by, false);
-                    taskManager.addTask(newDeadline);
-                    break;
-
-                case "event":
-                    counter++;
-                    // Split by " /from " and " /to "
-                    // Example: "project meeting /from Mon 2pm /to 4pm"
-                    int fromIndex = arguments.indexOf(" /from ");
-                    int toIndex = arguments.indexOf(" /to ");
-                    
-                    String eventDesc = arguments.substring(0, fromIndex);
-                    String from = arguments.substring(fromIndex + 7, toIndex);
-                    String to = arguments.substring(toIndex + 5);
-
-                    Event newEvent = new Event(counter, eventDesc, from, to, false);
-                    taskManager.addTask(newEvent);
-                    break;
-
-                default:
-                    System.out.println("I'm sorry, but I don't know what that means :-(");
-                    break;
+                System.out.println("---------------------------------------");
+                
+                Task task;
+                validateCommand(command);
+                validateTask(arguments);
+                switch(command) {
+                    case "bye": 
+                        break;
+    
+                    case "list": 
+                        System.out.printf("Here are the tasks in your list:\n");
+                        taskManager.printAllTask();
+                        break;
+    
+                    case "mark":
+                        validateTask(arguments);
+                        task = taskManager.findTask(Integer.parseInt(arguments));
+                        if (task != null) {
+                            task.setIsDone(true);
+                            System.out.printf("Nice! I have marked this task as done:\n%s\n", task.toString());
+                        }
+                        break;
+    
+                    case "unmark":
+                        validateTask(arguments);
+                        task = taskManager.findTask(Integer.parseInt(arguments));
+                        if (task != null) {
+                            task.setIsDone(false);
+                            System.out.printf("Nice! I have marked this task as not done yet:\n%s\n", task.toString());
+                        }
+                        break;
+    
+                    case "todo":
+                        validateTask(arguments);
+                        counter++;
+                        // arguments contains the description
+                        ToDo newTodo = new ToDo(counter, arguments, false);
+                        taskManager.addTask(newTodo);
+                        break;
+    
+                    case "deadline":
+                        validateTask(arguments);
+                        counter++;
+                        // Split by " /by "
+                        // Example: "return book /by Sunday"
+                        String[] deadlineParts = arguments.split(" /by ");
+                        String deadlineDesc = deadlineParts[0];
+                        String by = deadlineParts[1];
+                        
+                        Deadline newDeadline = new Deadline(counter, deadlineDesc, by, false);
+                        taskManager.addTask(newDeadline);
+                        break;
+    
+                    case "event":
+                        validateTask(arguments);
+                        counter++;
+                        // Split by " /from " and " /to "
+                        // Example: "project meeting /from Mon 2pm /to 4pm"
+                        int fromIndex = arguments.indexOf(" /from ");
+                        int toIndex = arguments.indexOf(" /to ");
+                        
+                        String eventDesc = arguments.substring(0, fromIndex);
+                        String from = arguments.substring(fromIndex + 7, toIndex);
+                        String to = arguments.substring(toIndex + 5);
+    
+                        Event newEvent = new Event(counter, eventDesc, from, to, false);
+                        taskManager.addTask(newEvent);
+                        break;
+    
+                    default:
+                        validateCommand(command);
+                        break;
+                }
+            } catch (TommyException e) {
+                System.out.println(e.getMessage());
             }
+
             
             System.out.println("---------------------------------------");
         } while (!userInput.equalsIgnoreCase("bye"));
 
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("---------------------------------------");
+        inputObj.close();
     }
+
+    private static void validateTask(String args) throws TommyException {
+        if (args.isBlank()) {
+            throw new TommyException("Please specify a task.");
+        }
+    }
+    private static void validateCommand(String args) throws TommyException {
+        Boolean isValid = false;
+        List<String> commandList = List.of("bye", "list", "mark", "unmark", "todo", "deadline", "event");
+
+        if (args.isBlank()) {
+            throw new TommyException("Please enter a command!");
+        }
+        for (String command: commandList) {
+            if (command.equalsIgnoreCase(args)) {
+                isValid = true;
+                break;
+            }
+        }
+        if (isValid == false) {
+            throw new TommyException("Please use a valid command!");
+        }
+    }
+    // TODO validate other task type
 }
---
+++ src/main/java/TommyException.java
@@ -0,0 +1,8 @@
+
+
+public class TommyException extends Exception {
+
+    public TommyException(String message) {
+        super(message);
+    }
+}
